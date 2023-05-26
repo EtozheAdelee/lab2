3c5ea2e6e6b15569ef21fdd8b420976eccd7859a
EtozheAdelee <134599678+EtozheAdelee@users.noreply.github.com>
2023-05-26 16:08:23 +0400

    Add files via upload

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'create_web_page_with_text.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_templates.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
